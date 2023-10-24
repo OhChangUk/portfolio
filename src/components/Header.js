1ef2029013266e9cd7e980f94b92b312998cb8ef
오창욱 <o2556@naver.com>
2023-10-24 16:27:46 +0900

    애니메이션 오류 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ import 'animate.css'
 function Header() {
   return (
     <>
+      <div className="w-full overflow-hidden">
         <img src="./../images/main.jpg" alt="main" className='custom-animation' />
+      </div>
     </>
   )
 }

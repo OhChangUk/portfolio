372f55c37fce6994293c7dfeaf56fc62d19b0881
오창욱 <o2556@naver.com>
2023-11-02 16:34:29 +0900

    모바일 테스트

--- src/components/Project.js
+++ src/components/Project.js
@@ -59,7 +59,7 @@ function Project({domRef}) {
                             )
                         })
                     }
-                    <select onChange={(e) => {setIsActive(e.target.value); setType(menuArray[e.target.value]);}} className='border border-black outline-none rounded-[5px] p-[6px] md:hidden mt-20 bg-white'>
+                    <select onChange={(e) => {setIsActive(e.target.value); setType(menuArray[e.target.value]);}} className='border border-black outline-none rounded-[5px] p-[6px] flex items-center md:hidden mt-20 bg-white'>
                         {
                             menuArray.map((e,i)=>{
                                 return(

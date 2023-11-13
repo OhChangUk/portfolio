3d070551fb02ea2b6a51d834c850a08182aa4608
오창욱 <o2556@naver.com>
2023-11-13 09:28:19 +0900

    푸터 이미지 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer({themeColor, theme}) {
     <>
         <div className={`${themeColor[theme].bg} py-14  w-full`}>
             <NavLink to={"https://github.com/OhChangUk"} target='_blank' className="">
-                <img src="./../images/skill6.png" alt="github" className='w-14 mx-auto mb-5' />
+                <img src="./../images/skill9.png" alt="github" className='w-14 mx-auto mb-5' />
             </NavLink>
             <p className='text-center'>ⓒ 2023. ChangUk Oh all rights reserved.</p>
         </div>

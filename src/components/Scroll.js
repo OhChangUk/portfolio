88b2188f8146047e52775faa694c100d918bf2f8
오창욱 <o2556@naver.com>
2023-11-17 12:30:21 +0900

    스크롤 버튼 호버 색상 추가

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -13,7 +13,7 @@ function Scroll({themeColor, theme}) {
   return (
     <>
         <div className="fixed z-10 right-[3%] bottom-[5%] opacity-80">
-            <div className={`text-center rounded-full ${themeColor[theme].buttonBg}`}>
+            <div className={`text-center rounded-full ${themeColor[theme].buttonBg} ${themeColor[theme].hoverBtn}`}>
                 <button className='lg:py-3.5 py-2.5 lg:px-4 px-3' onClick={ScrollTop}>
                     <FontAwesomeIcon icon={faAngleUp} size='xl' color='white'/>
                 </button>

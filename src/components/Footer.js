f70f2f6a76c7c705f3132553054e74e10b65ff6f
오창욱 <o2556@naver.com>
2023-11-22 11:46:54 +0900

    푸터 이메일 복사 기능 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,24 @@
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 function Footer({themeColor, theme}) {
+  const copyEmail = async (email) => {
+    await navigator.clipboard.writeText(email);
+    alert("이메일 복사에 성공했습니다");
+  };
   return (
     <>
         <div className={`${themeColor[theme].bg} py-14  w-full`}>
+          <div className="flex justify-around mb-5 w-40 mx-auto">
             <NavLink to={"https://github.com/OhChangUk"} target='_blank' className="">
-                <img src="./../images/skill9.png" alt="github" className='w-14 mx-auto mb-5' />
+                <img src="./../images/skill9.png" alt="github" className='w-14 mx-auto' />
             </NavLink>
-            <p className='text-center'>ⓒ 2023. ChangUk Oh all rights reserved.</p>
+            <FontAwesomeIcon icon={faEnvelope} onClick={()=>{copyEmail("o2556@naver.com")}} className='w-14 h-full cursor-pointer' />
+          </div>
+          <p className='text-center'>ⓒ 2023. ChangUk Oh all rights reserved.</p>
         </div>
     </>
   )
